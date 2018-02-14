d0168942f1dfc008431f843a00313552d11ed098
DESKTOP-72Q1U3S\Sam <hebertsa07@gmail.com>
2018-02-14 16:09:15 -0500

    writing tests

--- test.py
+++ test.py
@@ -1,22 +1,30 @@
-import os
+from server import app
+# import os
 import unittest
-import tempfile
-import server
+# import tempfile
 
 
 
 class BaseTestCase(unittest.TestCase):
-    def setUp(self):
-        server.app.config['TESTING'] = True
-        self.app = server.app.test_client()
+    # def setUp(self):
+    #     # server.app.config['TESTING'] = True
+    #     # self.app = server.app.test_client()
+    #     pass
 
 
-    def tearDown(self):
-        pass
+    # def tearDown(self):
+    #     pass
 
-    def test_simple(self):
-        res = self.app.get('/', follow_redirects=True)
+    def test_index(self):
+        # app.config['TESTING'] = True
+        tester = app.test_client(self)
+        res = tester.get('/')
         self.assertEqual(res.status_code, 200)
+        # res = self.app.get('/', follow_redirects=True)
+        # self.assertEqual(res.status_code, 200)
+        # self.assertTrue(True)
 
     if __name__ == '__main__':
         unittest.main()
+
+BaseTestCase()
